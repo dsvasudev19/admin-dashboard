ec97945c30e06ce712340b4bbeaa25ad4de05590
dsvasudev19 <vasudevds1729@gmail.com>
2024-10-01 19:54:27 +0530

    updated

--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -2,13 +2,17 @@
 import {
   Bell,
   BookUser,
+  ChevronDown,
   CircleUser,
   Container,
+  Dot,
   Home,
+  Icon,
   LineChart,
   Menu,
   Package,
   Package2,
+  PiIcon,
   ScanEye,
   Search,
   ShoppingCart,
@@ -35,12 +39,21 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { Outlet } from "react-router-dom";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { Link, Outlet } from "react-router-dom";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 export const description =
   "A products dashboard with a sidebar navigation and a main content area. The dashboard has a header with a search input and a user menu. The sidebar has a logo, navigation as, and a card with a call to action. The main content area shows an empty state with a call to action.";
 
 export function Dashboard() {
+  const [activeMenu,setActiveMenu]=useState("");
+
   return (
     <div className="grid min-h-screen max-w-[100%] md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
@@ -59,28 +72,307 @@ export function Dashboard() {
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
               <a
                 href="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                className="flex items-center gap-3 rounded-lg px-4 py-2 text-muted-foreground transition-all hover:text-primary"
               >
                 <Home className="h-4 w-4" />
                 Dashboard
               </a>
-              <a
-                href="/admin/vendor/"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+
+              <Collapsible
+                open={activeMenu === "vendor" ? true : false}
+                onOpenChange={() => {
+                  if (activeMenu !== "vendor") {
+                    setActiveMenu("vendor");
+                  } else {
+                    setActiveMenu("");
+                  }
+                }}
+                className="w-full"
               >
-                <UserCog className="h-4 w-4" />
-                Vendors
-              </a>
-              <a
+                <CollapsibleTrigger
+                  className="[&[data-state=open]>div>div>svg]:rotate-180 mb-1"
+                  asChild
+                >
+                  <Button
+                    variant={activeMenu === "vendor" ? "default" : "ghost"}
+                    className="w-full justify-start h-10"
+                  >
+                    <div className="w-full items-center flex justify-between">
+                      <div className="flex items-center">
+                        <span className="mr-4">
+                          <UserCog size={18} />
+                        </span>
+                        <p className={cn("max-w-[150px] truncate")}>
+                          Vendor Management
+                        </p>
+                      </div>
+                      <div className={cn("whitespace-nowrap")}>
+                        <ChevronDown
+                          size={18}
+                          className="transition-transform duration-200"
+                        />
+                      </div>
+                    </div>
+                  </Button>
+                </CollapsibleTrigger>
+                <CollapsibleContent className="overflow-hidden data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down ">
+                  <Button
+                    className="w-full justify-start h-10 mb-1 bg-white text-primary hover:bg-gray-300 hover:text-primary"
+                    asChild
+                  >
+                    <Link to={"/admin/vendor/"}>
+                      <span className="mr-4 ml-2">
+                        <Dot size={18} />
+                      </span>
+                      <p className={""}>Vendors</p>
+                    </Link>
+                  </Button>
+                  <Button
+                    className="w-full justify-start h-10 mb-1 bg-white text-primary border-none hover:bg-gray-300 hover:text-primary"
+                    asChild
+                  >
+                    <Link to={"/"}>
+                      <span className="mr-4 ml-2">
+                        <Dot size={18} />
+                      </span>
+                      <p className={""}>Add Vendor</p>
+                    </Link>
+                  </Button>
+                </CollapsibleContent>
+              </Collapsible>
+              {/* <a
                 href="/admin/store/"
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
               >
                 <Container className="h-4 w-4" />
                 Stores
-              </a>
+              </a> */}
+              <Collapsible
+                open={activeMenu === "store"}
+                onOpenChange={() => {
+                  if (activeMenu !== "store") {
+                    setActiveMenu("store");
+                  } else {
+                    setActiveMenu("");
+                  }
+                }}
+                className="w-full"
+              >
+                <CollapsibleTrigger
+                  className="[&[data-state=open]>div>div>svg]:rotate-180 mb-1"
+                  asChild
+                >
+                  <Button
+                    variant={activeMenu === "store" ? "default" : "ghost"}
+                    className="w-full justify-start h-10"
+                  >
+                    <div className="w-full items-center flex justify-between">
+                      <div className="flex items-center">
+                        <span className="mr-4">
+                          <Container size={18} />
+                        </span>
+                        <p className={cn("max-w-[150px] truncate")}>
+                          Store Management
+                        </p>
+                      </div>
+                      <div className={cn("whitespace-nowrap")}>
+                        <ChevronDown
+                          size={18}
+                          className="transition-transform duration-200"
+                        />
+                      </div>
+                    </div>
+                  </Button>
+                </CollapsibleTrigger>
+                <CollapsibleContent className="overflow-hidden data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down ">
+                  <Button
+                    className="w-full justify-start h-10 mb-1 bg-white text-primary hover:bg-gray-300 hover:text-primary"
+                    asChild
+                  >
+                    <Link to={"/admin/store/"}>
+                      <span className="mr-4 ml-2">
+                        <Dot size={18} />
+                      </span>
+                      <p className={""}>All Stores</p>
+                    </Link>
+                  </Button>
+                  <Button
+                    className="w-full justify-start h-10 mb-1 bg-white text-primary border-none hover:bg-gray-300 hover:text-primary"
+                    asChild
+                  >
+                    <Link to={"/"}>
+                      <span className="mr-4 ml-2">
+                        <Dot size={18} />
+                      </span>
+                      <p className={""}>Add Store</p>
+                    </Link>
+                  </Button>
+                </CollapsibleContent>
+              </Collapsible>
+
+              {/* <a
+                href="/admin/product/"
+                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+              >
+                <Package className="h-4 w-4" />
+                Products{" "}
+              </a> */}
+              <Collapsible
+                open={activeMenu === "product"}
+                onOpenChange={() => {
+                  if (activeMenu !== "product") {
+                    setActiveMenu("product");
+                  } else {
+                    setActiveMenu("");
+                  }
+                }}
+                className="w-full"
+              >
+                <CollapsibleTrigger
+                  className="[&[data-state=open]>div>div>svg]:rotate-180 mb-1"
+                  asChild
+                >
+                  <Button
+                    variant={activeMenu === "product" ? "default" : "ghost"}
+                    className="w-full justify-start h-10"
+                  >
+                    <div className="w-full items-center flex justify-between">
+                      <div className="flex items-center">
+                        <span className="mr-4">
+                          <Package size={18} />
+                        </span>
+                        <p className={cn("max-w-[150px] truncate")}>
+                          Product Management
+                        </p>
+                      </div>
+                      <div className={cn("whitespace-nowrap")}>
+                        <ChevronDown
+                          size={18}
+                          className="transition-transform duration-200"
+                        />
+                      </div>
+                    </div>
+                  </Button>
+                </CollapsibleTrigger>
+                <CollapsibleContent className="overflow-hidden data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down ">
+                  <Button
+                    className="w-full justify-start h-10 mb-1 bg-white text-primary hover:bg-gray-300 hover:text-primary"
+                    asChild
+                  >
+                    <Link to={"/admin/product/"}>
+                      <span className="mr-4 ml-2">
+                        <Dot size={18} />
+                      </span>
+                      <p className={""}>All Products</p>
+                    </Link>
+                  </Button>
+                  <Button
+                    className="w-full justify-start h-10 mb-1 bg-white text-primary border-none hover:bg-gray-300 hover:text-primary"
+                    asChild
+                  >
+                    <Link to={"/admin/product/"}>
+                      <span className="mr-4 ml-2">
+                        <Dot size={18} />
+                      </span>
+                      <p className={""}>Add Product</p>
+                    </Link>
+                  </Button>
+                  <Button
+                    className="w-full justify-start h-10 mb-1 bg-white text-primary border-none hover:bg-gray-300 hover:text-primary"
+                    asChild
+                  >
+                    <Link to={"/admin/product/"}>
+                      <span className="mr-4 ml-2">
+                        <Dot size={18} />
+                      </span>
+                      <p className={""}>Edit Product</p>
+                    </Link>
+                  </Button>
+                </CollapsibleContent>
+              </Collapsible>
+              {/* <a
+                href="/admin/user/"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <BookUser className="h-4 w-4" />
+                Employee Management
+              </a> */}
+              <Collapsible
+                open={activeMenu === "employee"}
+                onOpenChange={() => {
+                  if (activeMenu !== "employee") {
+                    setActiveMenu("employee");
+                  } else {
+                    setActiveMenu("");
+                  }
+                }}
+                className="w-full"
+              >
+                <CollapsibleTrigger
+                  className="[&[data-state=open]>div>div>svg]:rotate-180 mb-1"
+                  asChild
+                >
+                  <Button
+                    variant={activeMenu === "employee" ? "default" : "ghost"}
+                    className="w-full justify-start h-10"
+                  >
+                    <div className="w-full items-center flex justify-between">
+                      <div className="flex items-center">
+                        <span className="mr-4">
+                          <BookUser size={18} />
+                        </span>
+                        <p className={cn("max-w-[150px] truncate")}>
+                          Employee Management
+                        </p>
+                      </div>
+                      <div className={cn("whitespace-nowrap")}>
+                        <ChevronDown
+                          size={18}
+                          className="transition-transform duration-200"
+                        />
+                      </div>
+                    </div>
+                  </Button>
+                </CollapsibleTrigger>
+                <CollapsibleContent className="overflow-hidden data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down ">
+                  <Button
+                    className="w-full justify-start h-10 mb-1 bg-white text-primary hover:bg-gray-300 hover:text-primary"
+                    asChild
+                  >
+                    <Link to={"/admin/user/"}>
+                      <span className="mr-4 ml-2">
+                        <Dot size={18} />
+                      </span>
+                      <p className={""}>All Employees</p>
+                    </Link>
+                  </Button>
+                  <Button
+                    className="w-full justify-start h-10 mb-1 bg-white text-primary border-none hover:bg-gray-300 hover:text-primary"
+                    asChild
+                  >
+                    <Link to={"/admin/user/"}>
+                      <span className="mr-4 ml-2">
+                        <Dot size={18} />
+                      </span>
+                      <p className={""}>Add Employee</p>
+                    </Link>
+                  </Button>
+                  <Button
+                    className="w-full justify-start h-10 mb-1 bg-white text-primary border-none hover:bg-gray-300 hover:text-primary"
+                    asChild
+                  >
+                    <Link to={"/admin/user/"}>
+                      <span className="mr-4 ml-2">
+                        <Dot size={18} />
+                      </span>
+                      <p className={""}>Edit Employee Details</p>
+                    </Link>
+                  </Button>
+                </CollapsibleContent>
+              </Collapsible>
               <a
                 href="/admin/order/"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                className="flex items-center gap-3 rounded-lg px-4 py-2 text-muted-foreground transition-all hover:text-primary"
               >
                 <ShoppingCart className="h-4 w-4" />
                 Orders
@@ -89,36 +381,23 @@ export function Dashboard() {
                 </Badge>
               </a>
               <a
-                href="/admin/product/"
-                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
-              >
-                <Package className="h-4 w-4" />
-                Products{" "}
-              </a>
-              <a
                 href="/admin/user/"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                className="flex items-center gap-3 rounded-lg px-4 py-2 text-muted-foreground transition-all hover:text-primary"
               >
                 <Users className="h-4 w-4" />
                 Customers
               </a>
               <a
                 href="/admin/user/"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                className="flex items-center gap-3 rounded-lg px-4 py-2 text-muted-foreground transition-all hover:text-primary"
               >
                 <ScanEye className="h-4 w-4" />
                 Review Management
               </a>
-              <a
-                href="/admin/user/"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-              >
-                <BookUser className="h-4 w-4" />
-                Employee Management
-              </a>
+
               <a
                 href="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                className="flex items-center gap-3 rounded-lg px-4 py-2 text-muted-foreground transition-all hover:text-primary"
               >
                 <LineChart className="h-4 w-4" />
                 Analytics
--- src/pages/Products/ProductsListing.tsx
+++ src/pages/Products/ProductsListing.tsx
@@ -1,4 +1,3 @@
-
 import { Badge } from "@/components/ui/badge";
 
 import {
@@ -23,117 +22,75 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Field, Form, Formik } from "formik";
 
-
 const ProductsListing = () => {
   return (
     <div className="w-full border-2 border-slate-100 rounded-md overflow-scroll p-3">
-      <Tabs defaultValue="products" className="w-full">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="products">All Products</TabsTrigger>
-          <TabsTrigger value="password">Add Product</TabsTrigger>
-        </TabsList>
-        <TabsContent value="products">
-          <Table className="overflow-x-scroll">
-            <TableHeader>
-              <TableRow>
-                <TableHead className="hidden min-w-[100px] sm:table-cell">
-                  <span className="sr-only">img</span>
-                </TableHead>
-                <TableHead className="min-w-[100px]">Name</TableHead>
-                <TableHead className="min-w-[100px]">Status</TableHead>
-                <TableHead className="hidden sm:table-cell min-w-[100px]">
-                  Price
-                </TableHead>
-                <TableHead className="hidden sm:table-cell min-w-[100px]">
-                  Total Sales
-                </TableHead>
-                <TableHead className="hidden sm:table-cell min-w-[100px]">
-                  Created at
-                </TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow className="odd:bg-white even:bg-gray-200">
-                <TableCell className="hidden sm:table-cell">
-                  <img
-                    alt="Product img"
-                    className="aspect-square rounded-md object-cover"
-                    height="64"
-                    src="/product.jpeg"
-                    width="64"
-                  />
-                </TableCell>
-                <TableCell className="font-medium">
-                  Laser Lemonade Machine
-                </TableCell>
-                <TableCell>
-                  <Badge variant="danger">Draft</Badge>
-                </TableCell>
-                <TableCell className="hidden sm:table-cell">$499.99</TableCell>
-                <TableCell className="hidden sm:table-cell">25</TableCell>
-                <TableCell className="hidden sm:table-cell">
-                  2023-07-12 10:42 AM
-                </TableCell>
-              </TableRow>
-              <TableRow className="odd:bg-white even:bg-gray-100">
-                <TableCell className="hidden sm:table-cell">
-                  <img
-                    alt="Product img"
-                    className="aspect-square rounded-md object-cover"
-                    height="64"
-                    src="/product.jpeg"
-                    width="64"
-                  />
-                </TableCell>
-                <TableCell className="font-medium">
-                  Laser Lemonade Machine
-                </TableCell>
-                <TableCell>
-                  <Badge variant="success">Draft</Badge>
-                </TableCell>
-                <TableCell className="hidden sm:table-cell">$499.99</TableCell>
-                <TableCell className="hidden sm:table-cell">25</TableCell>
-                <TableCell className="hidden sm:table-cell">
-                  2023-07-12 10:42 AM
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TabsContent>
-        <TabsContent value="password">
-          <Card>
-            <CardHeader>
-              <CardTitle>Add Product</CardTitle>
-              <CardDescription>Add Yours Products here.</CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <Formik initialValues={{}} onSubmit={() => {}}>
-                {({ }) => (
-                  <Form>
-                    <div className="grid grid-cols-2 gap-4">
-                      <div className="grid gap-2">
-                        <div className="space-y-1">
-                          <Label htmlFor="name">Product Name</Label>
-                          <Field
-                            as={Input}
-                            name="name"
-                            id="name"
-                            type="text"
-                            placeholder="Product Name"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </Form>
-                )}
-              </Formik>
-            </CardContent>
-            <CardFooter>
-              <Button>Save Product</Button>
-            </CardFooter>
-          </Card>
-        </TabsContent>
-      </Tabs>
+      <Table className="overflow-x-scroll">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="hidden min-w-[100px] sm:table-cell">
+              <span className="sr-only">img</span>
+            </TableHead>
+            <TableHead className="min-w-[100px]">Name</TableHead>
+            <TableHead className="min-w-[100px]">Status</TableHead>
+            <TableHead className="hidden sm:table-cell min-w-[100px]">
+              Price
+            </TableHead>
+            <TableHead className="hidden sm:table-cell min-w-[100px]">
+              Total Sales
+            </TableHead>
+            <TableHead className="hidden sm:table-cell min-w-[100px]">
+              Created at
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow className="odd:bg-white even:bg-gray-200">
+            <TableCell className="hidden sm:table-cell">
+              <img
+                alt="Product img"
+                className="aspect-square rounded-md object-cover"
+                height="64"
+                src="/product.jpeg"
+                width="64"
+              />
+            </TableCell>
+            <TableCell className="font-medium">
+              Laser Lemonade Machine
+            </TableCell>
+            <TableCell>
+              <Badge variant="danger">Draft</Badge>
+            </TableCell>
+            <TableCell className="hidden sm:table-cell">$499.99</TableCell>
+            <TableCell className="hidden sm:table-cell">25</TableCell>
+            <TableCell className="hidden sm:table-cell">
+              2023-07-12 10:42 AM
+            </TableCell>
+          </TableRow>
+          <TableRow className="odd:bg-white even:bg-gray-100">
+            <TableCell className="hidden sm:table-cell">
+              <img
+                alt="Product img"
+                className="aspect-square rounded-md object-cover"
+                height="64"
+                src="/product.jpeg"
+                width="64"
+              />
+            </TableCell>
+            <TableCell className="font-medium">
+              Laser Lemonade Machine
+            </TableCell>
+            <TableCell>
+              <Badge variant="success">Draft</Badge>
+            </TableCell>
+            <TableCell className="hidden sm:table-cell">$499.99</TableCell>
+            <TableCell className="hidden sm:table-cell">25</TableCell>
+            <TableCell className="hidden sm:table-cell">
+              2023-07-12 10:42 AM
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 };
